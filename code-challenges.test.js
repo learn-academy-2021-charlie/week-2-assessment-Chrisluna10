95e80f8eed93119d173a649701a29b1258fd49af
Chrisluna10 <chrisluna4224@gmail.com>
2021-07-19 09:18:08 -0700

    week 2 assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,9 @@
 // ASSESSMENT 2: Coding practical questions with Jest
 
+const { expect } = require("@jest/globals")
+const { it, test } = require("jest-circus")
+const { describe } = require("yargs")
+
 // Please read all questions thoroughly
 // All problems can be solved with concepts covered in class or in the syllabus
 // Pseudo coding is REQUIRED
@@ -14,24 +18,56 @@
 
 // Reminder: The test will call your function
 
+
+
 // --------------------1) Create a function that takes a number as an argument and decides if the number is evenly divisble by three or not.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-var num1 = 15
+// var num1 = 15
 // Expected output: "15 is divisible by three"
+//write a test for a function that returns "15 is divisible by three."
 
-var num2 = 0
+//create a describe method that has the name of the function
+// describe("isDivisible", () => {
+    //use the test method that describes what will happen
+    // it("returns whether or not 15 is divisible by three", () => {
+        //expect method that has what the output should be
+        // expect(isDivisible("15 is divisible by three"));
+//     })
+// });
+
+  
+// var num2 = 0
 // Expected output: "0 is divisible by three"
 
-var num3 = -7
+// describe("isDivisible", () => {
+//     it("returns whether or not 0 is divisible by three", () => {
+//         expect(isDivisible("0 is divisible by three"));
+//     })
+// });
+
+// var num3 = -7
 // Expected output: "-7 is not divisible by three"
+
+describe("isDivisible", () => {
+    it("returns whether or not -7 is divisible by three", () => {
+        expect(isDivisible("-7 is divisible by three"));
+    })
+});
+
 
 
 
 // b) Create the function that makes the test pass.
-
-
+// const isDivisible = () => {
+    //if statement that takes an argument
+    // if (num1 % 3 === 0) {
+        //return output that should approve test
+//     return "15 is divisible by three"
+//    } 
+// }
+//I keep recieving an error that says "your test suite must contain at least one test."
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
@@ -40,6 +76,18 @@ var num3 = -7
 var randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
 
+describe("cap", () => {
+    test("returns all words capitalized", () => {
+    expect(cap(["STREETLAMP", "POTATO", "TEETH", "CONCLUSION", "NEPHEW"]).toEqual(["streetlamp", "potato", "teeth", "conclusion", "nephew"]))
+    })
+})
+
+const cap = (array) => {
+   let newArr = array.map(value => {
+    return value.toUpperCase() 
+   })
+   return newArr.join("")
+}
 var randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
 // Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
 
@@ -63,3 +111,5 @@ var vowelTester3 = "challenge"
 
 
 // b) Create the function that makes the test pass.
+
+//I was having a problem with testing but I still wanted to submit a pull request. 
--- coding-concepts.js
+++ coding-concepts.js
@@ -10,8 +10,8 @@
 var cohort = "Charlie 2021"
 // console.log(cohort.split(""))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: This will return an output of each index being its own string.
+// b) Verify and explain: .split with quotes splits each index up and makes it an array.
 
 
 // --------------------2) What will this log?
@@ -21,8 +21,8 @@ const greeter = (name) => {
 }
 // console.log(greeter("LEARN Student"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: There is no return so this will have an output of undefined.
+// b) Verify and explain: return has to be included before the string inside of the brackets in order to have an output of "Hello, LEARN student"
 
 
 // --------------------3) What will this log?
@@ -30,8 +30,8 @@ const greeter = (name) => {
 var multBy2 = [4, 5, 6, 7, 8].map(value => value * 2)
 // console.log(multBy2)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Each value in the array will double
+// b) Verify and explain: .map makes each value have an argument of the value * 2.
 
 
 // --------------------4) What will this log?
@@ -39,21 +39,26 @@ var multBy2 = [4, 5, 6, 7, 8].map(value => value * 2)
 var oddsOnly = [11, 12, 13, 14, 15].filter(value => value % 2 === 0)
 // console.log(oddsOnly)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: this will return an array of just odd numbers
+// b) Verify and explain: This returned an array of just the even numbers. I am assuming it is because the value % 2 should have strictly equaled 1 instead of 0.
 
 
 // --------------------5) What will this log?
 
-class Learn {
-  constructor(){
-    this.student = "George",
-    this.cohort = "Charlie",
-    this.year = 2021
-  }
-}
-var learnStudent = new Learn
+// class Learn {
+//   constructor(){
+//     this.student = "George",
+//     this.cohort = "Charlie",
+//     this.year = 2021
+//   }
+// }
+// var learnStudent = new Learn
 // console.log(learnStudent)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: 
+// "[
+//   student: "George"
+//   cohort: "Charlie"
+//   year: 2021
+// ]"
+// b) Verify and explain: the variable learnStudent is taking in the class of Learn which has an output of all the data within the learn class.
